3118b28488fcdb61f49ca64a73bb3e42090fb13c
Joe Tustin <J.Tustin@gmail.com>
2024-08-18 14:39:24 -0600

    name

--- script.sh
+++ script.sh
@@ -8,6 +8,8 @@ do
   NAME=$(buildkite-agent meta-data get NAME)
   echo "Script run by $NAME"
   buildkite-agent meta-data set "NAME" "$NAME"
+  NAME2=$(buildkite-agent meta-data get NAME)
+  echo "stored var $NAME2"
   sleep 5
 done
 
