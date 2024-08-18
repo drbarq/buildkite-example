ed0da39a1d254009dab18dfa23448098a5b7cad0
Joe Tustin <J.Tustin@gmail.com>
2024-08-18 14:34:10 -0600

    name

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ NAME=""
 until [ ${#NAME} -gt 0 ]
 do
   echo "Script is checking for name"
-  NAME=$(buildkite-agent meta-data get name)
+  NAME=$(buildkite-agent meta-data get NAME)
   echo "Script run by $NAME"
   buildkite-agent meta-data set "NAME" "$NAME"
   sleep 5
