d8483e47de8ad10d5a8b74ef5b229764425e3a3b
Joe Tustin <J.Tustin@gmail.com>
2024-08-18 17:27:53 -0600

    new pipe

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -2,4 +2,20 @@
 set -euo pipefail
 
 echo "Setting up the pipeline..."
-buildkite-agent pipeline upload .buildkite/dynamic_steps.yml
+
+buildkite-agent pipeline upload << YAML
+steps:
+  - block: ":thinking_face: What now?"
+    prompt: "Choose the next action"
+    fields:
+      - select: "Action"
+        key: "action"
+        options:
+          - label: ":input_numbers: Enter your name"
+            value: "enter_name"
+          - label: ":wave: Say hello"
+            value: "say_hello"
+
+  - label: ":robot_face: Process Input"
+    command: ".buildkite/process.sh"
+YAML
