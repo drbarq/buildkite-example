2a35fce84ddc102346975d143cb1a362a00ac967
Joe Tustin <J.Tustin@gmail.com>
2024-08-18 14:32:45 -0600

    name

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ do
   echo "Script is checking for name"
   NAME=$(buildkite-agent meta-data get name)
   echo "Script run by $NAME"
-  buildkite-agent meta-data set "name" "$NAME"
+  buildkite-agent meta-data set "NAME" "$NAME"
   sleep 5
 done
 
