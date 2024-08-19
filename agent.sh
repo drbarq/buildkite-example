cf30039dfa40fc805d745881166bc436810e4253
Joe Tustin <J.Tustin@gmail.com>
2024-08-19 08:57:04 -0600

    script

--- agent.sh
+++ agent.sh
@@ -4,16 +4,15 @@ set -euo pipefail
 DOCKER_USERNAME="your-docker-usernsdfsdafame"
 DOCKER_PASSWORD="your-docker-passwosadfasdfrd"
 
-until [ ${#NAME} -gt 0 ]
-do
-  echo "Script is checking for NAME"
-  NAME=$(buildkite-agent meta-data get NAME)
-  echo "Script run by $NAME"
-  buildkite-agent meta-data set "DOCKER_USERNAME" "$DOCKER_USERNAME"
-  buildkite-agent meta-data set "DOCKER_PASSWORD" "$DOCKER_PASSWORD"
-  DOCKER_USERNAME_BUILD=$(buildkite-agent meta-data get DOCKER_USERNAME)
-  DOCKER_PASSWORD_BUILD=$(buildkite-agent meta-data get DOCKER_PASSWORD)
-  echo "stored var $DOCKER_USERNAME_BUILD, $DOCKER_PASSWORD_BUILD"
-  sleep 5
-done
+
+echo "Script is checking for NAME"
+NAME=$(buildkite-agent meta-data get NAME)
+echo "Script run by $NAME"
+buildkite-agent meta-data set "DOCKER_USERNAME" "$DOCKER_USERNAME"
+buildkite-agent meta-data set "DOCKER_PASSWORD" "$DOCKER_PASSWORD"
+DOCKER_USERNAME_BUILD=$(buildkite-agent meta-data get DOCKER_USERNAME)
+DOCKER_PASSWORD_BUILD=$(buildkite-agent meta-data get DOCKER_PASSWORD)
+echo "stored var $DOCKER_USERNAME_BUILD, $DOCKER_PASSWORD_BUILD"
+sleep 5
+
 
