67a47d623579ad57a94c49f998bcb30643af1cf1
Joe Tustin <J.Tustin@gmail.com>
2024-08-19 08:55:58 -0600

    docker test

--- agent.sh
+++ agent.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ "$BUILDKITE_PIPELINE_SLUG" == "your-pipeline-slug" ]]; then
-  export DOCKER_USERNAME="your-docker-usernsdfsdafame"
-  export DOCKER_PASSWORD="your-docker-passwosadfasdfrd"
-fi
+DOCKER_USERNAME="your-docker-usernsdfsdafame"
+DOCKER_PASSWORD="your-docker-passwosadfasdfrd"
+
+until [ ${#NAME} -gt 0 ]
+do
+  echo "Script is checking for NAME"
+  NAME=$(buildkite-agent meta-data get NAME)
+  echo "Script run by $NAME"
+  buildkite-agent meta-data set "DOCKER_USERNAME" "$DOCKER_USERNAME"
+  buildkite-agent meta-data set "DOCKER_PASSWORD" "$DOCKER_PASSWORD"
+  DOCKER_USERNAME_BUILD=$(buildkite-agent meta-data get DOCKER_USERNAME)
+  DOCKER_PASSWORD_BUILD=$(buildkite-agent meta-data get DOCKER_PASSWORD)
+  echo "stored var $DOCKER_USERNAME_BUILD, $DOCKER_PASSWORD_BUILD"
+  sleep 5
+done
+
