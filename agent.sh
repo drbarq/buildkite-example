0e6b681df092821e87992f29ebf0ceb49b76ae41
Joe Tustin <J.Tustin@gmail.com>
2024-08-19 09:19:12 -0600

    adding in login

--- agent.sh
+++ agent.sh
@@ -5,14 +5,14 @@ DOCKER_USERNAME="jtustin"
 DOCKER_PASSWORD="SKEA_maug2eeff"
 
 
-echo "Script is checking for NAME"
-NAME=$(buildkite-agent meta-data get NAME)
-echo "Script run by $NAME"
-buildkite-agent meta-data set "DOCKER_USERNAME" "$DOCKER_USERNAME"
-buildkite-agent meta-data set "DOCKER_PASSWORD" "$DOCKER_PASSWORD"
-DOCKER_USERNAME_BUILD=$(buildkite-agent meta-data get DOCKER_USERNAME)
-DOCKER_PASSWORD_BUILD=$(buildkite-agent meta-data get DOCKER_PASSWORD)
-echo "stored var $DOCKER_USERNAME_BUILD, $DOCKER_PASSWORD_BUILD"
-sleep 5
+# echo "Script is checking for NAME"
+# NAME=$(buildkite-agent meta-data get NAME)
+# echo "Script run by $NAME"
+# buildkite-agent meta-data set "DOCKER_USERNAME" "$DOCKER_USERNAME"
+# buildkite-agent meta-data set "DOCKER_PASSWORD" "$DOCKER_PASSWORD"
+# DOCKER_USERNAME_BUILD=$(buildkite-agent meta-data get DOCKER_USERNAME)
+# DOCKER_PASSWORD_BUILD=$(buildkite-agent meta-data get DOCKER_PASSWORD)
+# echo "stored var $DOCKER_USERNAME_BUILD, $DOCKER_PASSWORD_BUILD"
+# sleep 5
 
 
